f191d6453ff5dfc65a6f4d70ab9f901b941b9605
Alejandro Briceño <terremoto_alejandro@hotmail.com>
2026-07-09 03:55:04 -0400

    ID en el token

--- backend/schemas/token_schema.py
+++ backend/schemas/token_schema.py
@@ -9,6 +9,7 @@ class TokenRequest(BaseModel):
 # Schema para respuesta de token renovado.
 class TokenResponse(BaseModel):
 
+    id_vendedor: int = Field(..., description="ID del vendedor autenticado")
     access_token: str = Field(..., description="Token de acceso renovado")
     token_type: Literal["bearer"] = "bearer"
     expires_in: int = Field(..., description="Tiempo en segundos hasta que el token de acceso expire")
